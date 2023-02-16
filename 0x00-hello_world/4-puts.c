5c997b1b8e771b792a1a089f7bbc005c83df6ca5
Oluwadare Ibitoye <darebty@gmail.com>
2023-02-16 14:13:54 -0800

    cisfun

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+** main - print the string in the put function 
+*
+** and output it in 4-puts.c
+**/
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+
+}
