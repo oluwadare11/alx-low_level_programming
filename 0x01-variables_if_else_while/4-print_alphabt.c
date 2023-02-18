8633e955b0791927d4374a7472dadec4e8c0b7a1
Oluwadare Ibitoye <darebty@gmail.com>
2023-02-17 16:47:36 -0800

    variables

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -5,14 +5,18 @@
  *
  * Return: Always 0.
  */
+
 int main(void)
 {
 	char letter;
+
 	for (letter = 'a'; letter <= 'z'; letter++)
 	{
 		if (letter != 'e' && letter != 'q')
 			putchar(letter);
 	}
+
 	putchar('\n');
+
 	return (0);
 }
