ed1e8daede8b4bec6d9a3e9d921ebe677a837982
Oluwadare Ibitoye <darebty@gmail.com>
2023-02-21 06:28:57 -0800

    functions

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include <main.h>
+
+/**
+ * main - function that prints the alphabet
+ * in lowercase, followed by a new line
+ *
+ * Return: Always 0
+ */
+
+void print_alphabet(void);
+{
+	char la;
+
+	for (la = 'a'; la <= 'z'; la++)
+		_putchar(la);
+	_putchar('\n');
+}
