ad7f8beb37d22efe2fcf5c07eed3ad53997426aa
Oluwadare Ibitoye <darebty@gmail.com>
2023-02-21 07:04:09 -0800

    functions

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include <main.h>
+
+/**
+ * main - prints 10 times the alphabet
+ * in lowercase, followed by a new line
+ *
+ * Return: Always 0
+ */
+
+void print_alphabet_x10(void);
+{
+	int ten;
+	char la;
+
+	for (ten = 0; ten <= 9; ten++)
+	{
+		for (la = 'a'; la <= 'z'; la++)
+			_putchar la;
+		_putchar('\n');
+	}
+}
