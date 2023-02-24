b66bdeaba59ff962e9564b9f806c0f4b0e4503da
Oluwadare Ibitoye <darebty@gmail.com>
2023-02-24 12:13:29 -0800

    debugg

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -11,12 +11,18 @@
 int largest_number(int a, int b, int c)
 {
 	int largest;
-
-	if (a >= b && a >= c)
-	{
+	
+	if (a > b && a > c)
+	
+	{ 
 		largest = a;
 	}
-	else if (b >= a && b >= c)
+	
+	else if (a > b && c > a)
+	{
+		largest = c;
+	}
+	else if (b > c)
 	{
 		largest = b;
 	}
@@ -24,6 +30,5 @@ int largest_number(int a, int b, int c)
 	{
 		largest = c;
 	}
-
 	return (largest);
 }
