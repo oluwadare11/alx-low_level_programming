965ad6394623d193595c543ccff8e04902180992
Oluwadare Ibitoye <darebty@gmail.com>
2023-02-27 10:25:55 -0800

    pointers

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * puts2 - prints every other character of a string, 
+ * puts2 - prints every other character of a string,
  * starting with the first character,
  * followed by a new line
  * @str: The string to be treated
