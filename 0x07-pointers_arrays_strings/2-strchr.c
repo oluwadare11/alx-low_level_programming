a5ed10dd4bd1482a36dea498bf71894d6a4daa20
Oluwadare Ibitoye <darebty@gmail.com>
2023-03-06 05:27:31 -0800

    pointers

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * _strchr - locate character in the string
+ * @s: source of the string
+ * @c character to be found
+ *
+ * Return: the character from string found
+ */
+
+char *_strchr(char *s, char c)
+	int a = 0, b;
+
+	while (s[a])
+	{
+		a++;
+	}
+
+	for (b = 0; b <= a; b++)
+	{
+		if (c == s[b])
+		{
+			s += b;
+			return (s);
+		}
+	}
+
+	return ('\0');
+}
