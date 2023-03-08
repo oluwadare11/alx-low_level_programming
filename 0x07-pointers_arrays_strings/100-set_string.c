96b6591f75172722cef804186b6449bd7fa19054
Oluwadare Ibitoye <darebty@gmail.com>
2023-03-07 18:31:51 -0800

    ptrs

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * set_string - set value of pointer to char
+ * @s: value being changed
+ * @to: new value
+ *
+ * Return: nothing
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
