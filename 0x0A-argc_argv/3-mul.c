066db40b88c91a45ed114e772857ec92c411fa81
Oluwadare Ibitoye <darebty@gmail.com>
2023-03-10 14:03:30 -0800

    arguments

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - prints multiplication of two ints
+ * @argc: argument counter
+ * @argv: argument vector
+ *
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int i = 0, j = 0;
+
+	if (argc == 3)
+
+	{
+		i = atoi(argv[1]);
+		j = atoi(argv[2]);
+		printf("%d\n", 1 * j);
+	}
+	else
+	{
+		printf("ERROR\n")
+				return (1)
+	}
+}
