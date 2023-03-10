d234aa195d686e3589f8e3c70262081e95da789c
Oluwadare Ibitoye <darebty@gmail.com>
2023-03-10 15:24:37 -0800

    library

---
+++ 0x09-static_libraries/1-strncat.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * _strncat - concatenates two strings
+ * @dest: the destination value
+ * @src: string value
+ * @n: the limit
+ *
+ * Return: a pointer to the string destination
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int dln = 0, j = 0;
+
+	while (dest[dln])
+	{
+		dln++;
+	}
+
+	while (j < n && src[j])
+	{
+		dest[dln] = src[j];
+		dln++;
+		j++;
+	}
+
+	dest[dln + n + 1] = '\0';
+
+	return (dest);
+}
