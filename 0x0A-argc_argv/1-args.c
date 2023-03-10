a3686947064f19954847154d5fa0781ae8d98d3c
Oluwadare Ibitoye <darebty@gmail.com>
2023-03-10 13:08:17 -0800

    argument

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - prints the no of arguments supplied
+ * @argc: argument counter
+ * @argv: argument vector
+ *
+ * Return: integer value
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc);
+
+	return (0);
+
+}
