f09c0575d174dd368b256254fdd658977f3fbc7c
Oluwadare Ibitoye <darebty@gmail.com>
2023-03-10 17:35:08 -0800

    lib

--- 0x09-static_libraries/_putchar.c
+++ 0x09-static_libraries/_putchar.c
@@ -1,24 +1,15 @@
 #include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 /**
- * main - prints _putchar followed by a new line
- *
+ * _putchar - prints _putchar followed by a new line
+ * @c: char to be returned
  * Return: Always 0
  */
 
-int _putchar(char c);
-int main(void)
+int _putchar(char c)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
-
 	return (0);
-
 }
