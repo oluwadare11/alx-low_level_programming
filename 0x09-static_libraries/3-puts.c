ccbe6a90384d5818f1263a7342282819c61572a1
Oluwadare Ibitoye <darebty@gmail.com>
2023-03-10 16:33:40 -0800

    library

---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+** main - print the string in the put function
+** Return:0
+** and output it
+**/
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+
+}
