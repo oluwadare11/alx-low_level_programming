a16ac29b4477cfe875268d0d95876a0e08c702ae
Oluwadare Ibitoye <darebty@gmail.com>
2023-03-20 15:40:12 -0700

    pp

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,20 @@
+#include "dog.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * free_dog - .....
+ * @d: ....
+ *
+ * Return: 0
+ */
+
+void free_dog(dog_t *d)
+{
+	if (d != NULL)
+	{
+		free(d->owner);
+		free(d->name);
+		free(d);
+	}
+}
