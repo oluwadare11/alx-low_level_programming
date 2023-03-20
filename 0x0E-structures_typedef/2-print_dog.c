d4037758b1f039e5a755d362641830e1198eb0f6
Oluwadare Ibitoye <darebty@gmail.com>
2023-03-20 14:51:23 -0700

    str

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_dog - prints struct dog
+ * @d: dog structure
+ *
+ * Return: 0
+ */
+
+void print_dog(struct dog *d)
+
+{
+	{
+	if (d != NULL)
+	{
+		printf("Name: %s\n", (d->name) ? d->name : "(nil)");
+		printf("Age: %f\n", (d->age) ? d->age : 0);
+		printf("Owner: %s\n", (d->owner) ? d->owner : "(nil)");
+	}
+}
+}
