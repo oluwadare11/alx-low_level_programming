2e8280d3a7289e2446fd3bc3cbe98ca543bfad63
Oluwadare Ibitoye <darebty@gmail.com>
2023-03-22 10:17:20 -0700

    fnp

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,16 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - function to print a name
+ *
+ * @name: name to print
+ * @f: pointer
+ *
+ * Return: 0
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (name != NULL && f != NULL)
+		f(name);
+}
