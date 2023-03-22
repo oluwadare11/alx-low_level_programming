9fc6b929073ed75a172ca9a92770224f84fa1553
Oluwadare Ibitoye <darebty@gmail.com>
2023-03-22 10:41:50 -0700

    pnf

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,9 +1,10 @@
 #include "function_pointers.h"
 
 /**
- * array_iterator - executes a function given as a parameter on each element of an array
+ * array_iterator - executes a function given
+ * as a parameter on each element of an array
  * @size: size of array
- * @array: ....
+ * @array: ...
  * @action: pointer to the function
  *
  */
