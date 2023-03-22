34afccc9aa672657f525e076b3d669630d4e12f8
Oluwadare Ibitoye <darebty@gmail.com>
2023-03-22 11:48:00 -0700

    fnp

---
+++ 0x0F-function_pointers/3-calc.h
@@ -0,0 +1,11 @@
+#ifndef _HEADER_
+#define _HEADER_
+
+int (*get_op_func(char *s))(int, int);
+int op_add(int a, int b);
+int op_sub(int a, int b);
+int op_mul(int a, int b);
+int op_div(int a, int b);
+int op_mod(int a, int b);
+
+#endif
