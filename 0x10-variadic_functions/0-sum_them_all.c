f013f32a6b3ccb5db563443c6185569cc1be3119
Oluwadare Ibitoye <darebty@gmail.com>
2023-03-23 11:52:39 -0700

    variadic

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -11,15 +11,23 @@
 
 int sum_them_all(const unsigned int n, ...)
 {
-	va_list ap;
-	unsigned int i, sum = 0;
+	va_list args;
+	unsigned int i = 0;
+	int sum = 0;
 
-	va_start(ap, n);
+	if (n != 0)
+	{
+		va_start(args, n);
 
-	for (i = 0, i < n, i++)
-		sum += va_arg(ap, int);
+		while (i < n)
+		{
+			sum += va_arg(args, int);
+			i++;
+		}
 
-	va_end(ap);
-	return (sum);
+		va_end(args);
+		return (sum);
+	}
 
+	return (0);
 }
