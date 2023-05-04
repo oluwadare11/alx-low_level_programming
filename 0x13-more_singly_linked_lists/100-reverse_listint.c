da998d7b348dfb0ec73f24c5e50015b9dbfaae81
Ibitoye Oluwadare <darebty@gmail.com>
2023-05-03 17:08:41 -0700

    additional tasks

---
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+  * reverse_listint - Reverse list
+  * @head: the start
+  *
+  * Return: ...
+  */
+
+listint_t *reverse_listint(listint_t **head)
+{
+	listint_t *prev = NULL, *next = NULL;
+
+	if (head)
+	{
+		while (*head)
+		{
+			next = *head;
+			*head = (*head)->next;
+			next->next = prev;
+			prev = next;
+		}
+
+		*head = prev;
+		return (*head);
+	}
+
+	return (NULL);
+}
